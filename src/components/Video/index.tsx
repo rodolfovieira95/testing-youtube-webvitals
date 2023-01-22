02b3fbbc521c6109a4ce71d3711c1ce7617c2925
Rodolfo Vieira <rodolfovieira95@outlook.com>
2023-01-22 02:28:30 -0300

    feat: add lazy laod video

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -11,6 +11,7 @@ export const Video = () => {
         // frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         // allowfullscreen
+        loading="lazy"
       ></iframe>
     </section>
   );
