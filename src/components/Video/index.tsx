3ffcb431c1486d704e0ede754f1ac2bb0dd526aa
Rodolfo Vieira <rodolfovieira95@outlook.com>
2023-01-22 02:53:57 -0300

    feat: add lite yt lib

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,9 +1,14 @@
 import styles from "./style.module.css";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 export const Video = () => {
   return (
     <section className={styles.container}>
-      <iframe
+      <div className={styles.video}>
+        <LiteYouTubeEmbed id="cCZKDiRUw7I" title="O que é IoT?" />
+      </div>
+      {/* <iframe
         width="560"
         height="315"
         src="https://www.youtube.com/embed/cCZKDiRUw7I"
@@ -12,7 +17,7 @@ export const Video = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         // allowfullscreen
         loading="lazy"
-      ></iframe>
+      ></iframe> */}
     </section>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "@styles/global.css";
-
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
